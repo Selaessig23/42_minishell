1c4828326b060f94cba61df963b20153b1d3c45a
Markus Stracke <mstracke@c4b11c1.42berlin.de>
2024-08-02 18:49:16 +0200

    fix[ft_lstadd_back.c]: fix segfault if lst == NULL;

--- libft/srcs_bonus/ft_lstadd_back.c
+++ libft/srcs_bonus/ft_lstadd_back.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mpeshko <mpeshko@student.42berlin.de>      +#+  +:+       +#+        */
+/*   By: mstracke <mstracke@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 00:54:14 by mpeshko           #+#    #+#             */
-/*   Updated: 2024/06/07 19:57:09 by mpeshko          ###   ########.fr       */
+/*   Updated: 2024/08/02 17:08:50 by mstracke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 // Adds the given element ‘new’ to the end of the linked list pointed
 // to by ‘lst’. If ‘lst’ is an empty list, ‘new’ becomes the first element.
-
+/*
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
@@ -23,4 +23,20 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	if (!last)
 		*lst = new;
 	last->next = new;
+}
+*/
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*curr;
+
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
+	curr = *lst;
+	while (curr->next != NULL)
+		curr = curr->next;
+	curr->next = new;
 }
