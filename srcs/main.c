40e40004e5c2e188fda35fd173bb058ac8453495
Markus Stracke <mstracke@c4b11c1.42berlin.de>
2024-08-08 16:25:26 +0200

    fix[main.c]: fix small valgrind bug if add *input

--- srcs/main.c
+++ srcs/main.c
@@ -29,32 +29,37 @@ int	main(int argc, char **argv)
 				// free session
 				exit(EXIT_FAILURE);
 			}
-			if (input)
+			if(*input)
 			{
-					add_history(input);
-					//integrate history_list for fun
+				if (input)
+				{
+						add_history(input);
+						//integrate history_list for fun
+				}
+				ft_printf("input length: %i\n", ft_strlen(input));
+				input_arr = create_nodes(input);
+				free(input);
+				input = NULL;
+				// ft_test_arr_print(input_arr, prompt);
+				// attempt to use both ways, to not destroy the work
+				// with input_arr on extra input of marina
+				// otherwise I would add this function call to create_nodes-function
+				// in lexer.c
+				// ft_printf("test0\n");
+				lexx = ft_tokenizer(input_arr);
+				// ft_printf("test1\n");
+				//comment out this free-function if ft_test_arr_print is in use
+				// ft_test_ll_print(lexx, prompt);
+				ft_free(input_arr);
+				//to seperate debug-infos of lexer from bash-output
+				ft_printf("\n");
+				if (!ft_syntax(lexx))
+					ft_test_ll_print(lexx, prompt);
+				else
+					ft_free_ll(&lexx);
 			}
-			ft_printf("input length: %i\n", ft_strlen(input));
-			input_arr = create_nodes(input);
-			free(input);
-			input = NULL;
-			// ft_test_arr_print(input_arr, prompt);
-			// attempt to use both ways, to not destroy the work
-			// with input_arr on extra input of marina
-			// otherwise I would add this function call to create_nodes-function
-			// in lexer.c
-			// ft_printf("test0\n");
-			lexx = ft_tokenizer(input_arr);
-			// ft_printf("test1\n");
-			//comment out this free-function if ft_test_arr_print is in use
-			// ft_test_ll_print(lexx, prompt);
-			ft_free(input_arr);
-			//to seperate debug-infos of lexer from bash-output
-			ft_printf("\n");
-			if (!ft_syntax(lexx))
-				ft_test_ll_print(lexx, prompt);
-			else
-				ft_free_ll(&lexx);
+			else if (!*input)
+				free(input);
 		}
 		ft_free_ll(&lexx);
 		rl_clear_history();
