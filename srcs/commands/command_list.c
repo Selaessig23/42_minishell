4e05881c8d653b8e380269e012e347359698d978
Markus Stracke <mstracke@c3a1c4.42berlin.de>
2024-09-16 16:12:43 +0200

    feat: create a list of commands

---
+++ srcs/commands/command_list.c
@@ -0,0 +1,43 @@
+
+#include "minishell.h"
+
+/**
+ * DESCRIPTION
+ * 
+ */
+
+/**
+ * @brief function to define infile and in_heredoc
+ */
+
+/**
+ * @brief function to define outfile and out_append
+ */
+
+/**
+ * @brief function to create array of commands
+ */
+
+/**
+ * @brief function to create a linked list of commands
+ */
+t_list	*ft_commands(t_list *lexx)
+{
+	t_list	*lexx_curr;
+	t_list	*comm;
+	t_list	*comm_curr;
+	t_lexer	*token;
+
+	lexx_curr = lexx;
+	token == ((t_lexer)->content)->token;
+	//function to init comm
+	comm_curr = comm;
+	while (curr != NULL)
+	{
+
+		if (token == 1 || token == 2)
+
+		curr = curr->next;
+		token == ((t_lexer)->content)->token;
+	}
+}
