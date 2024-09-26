e5bd70ffced58f915c96091e05855c86cf0dae9f
Markus Stracke <mstracke@c4b11c1.42berlin.de>
2024-09-26 12:30:37 +0200

    fix small issue if echo has no input

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -26,22 +26,24 @@ void    ft_echo(t_data *comm_info, t_big *big)
 	argv = comm_info->cmd;
 	no_nl = 0;
 	fd = comm_info->fd_outfile;
-	// printf("fd-check: %i\n", fd);
-	argv += 1;
+	if (argv + 1 && *(argv + 1))
+		argv += 1;
 	if (!ft_strncmp(*argv, "-n", ft_strlen(*argv)))
 	{
 		no_nl = true;
 		argv += 1;
 	}
+	// printf("fd-check: %i\n", fd);
 	while (*argv)
 	{
-		ft_putstr_fd(*argv, fd);
+		// ft_putstr_fd(*argv, fd);
 		// printf("to print -check: %s\n",*argv);
 		// dprintf(fd, "print: %s", *argv);
 		argv += 1;
 		if (*argv)
 			ft_putchar_fd(' ', fd);
 	}
+	// printf("fd-check 2: %i\n", fd);
 	if (no_nl == 0)
 		ft_putchar_fd('\n', fd);
 	if (fd > 2)
